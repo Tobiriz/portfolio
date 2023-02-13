d6de6a613030714fc19581983592b54955529852
Tobias <tobias.weinlich@gmx.de>
2023-02-13 02:52:34 +0100

    Started content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,8 +9,50 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Home</h1>
-        <p>Home page</p>
+    <div class="body">
+        <div class="about">
+            <h1>Tobias Weinlich</h1>
+            <h3>Frontend Developer</h3>
+
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    background-color: #191919;
+    overflow-x: hidden;
+
+    h1, h2, h3,
+    h4, h5, p {
+        color: #fff;
+    }
+
+    .about {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        align-self: flex-end;
+        gap: 1rem;
+        padding: 2rem;
+
+        h1 {
+            font-size: 3rem;
+            font-weight: 700;
+            text-transform: uppercase;
+            letter-spacing: 0.1rem;
+        }
+
+        h3 {
+            font-size: 1.5rem;
+            font-weight: 700;
+            text-transform: uppercase;
+            letter-spacing: 0.1rem;
+        }
+    }
+}
+</style>
