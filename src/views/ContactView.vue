851bef7b2e4f1f5b4cc73742538c3f15c1c8faf0
Tobias <tobias.weinlich@gmx.de>
2023-02-14 22:17:26 +0100

    Findished Contact Info

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,8 +10,26 @@ export default {
 
 <template>
     <div>
-        <div>
-            Tobias Weinlich
+        <div class="contact-container">
+            <div class="contact-container__section">
+                <h1>E-MAIL</h1>
+                <a href="mailto:tobias.weinlich@mail.de">
+                    <h2>tobias.weinlich@mail.de</h2>
+                </a>
+            </div>
+
+            <div class="contact-container__section">
+                <h1>MEDIA</h1>
+                <a href="https://github.com/Tobiriz" target="_blank">
+                    <h2>GitHub</h2>
+                </a>
+                <a href="https://www.linkedin.com/in/tobias-weinlich-34aba0260/" target="_blank">
+                    <h2>XING</h2>
+                </a>
+                <a href="https://www.xing.com/profile/Tobias_Weinlich/cv" target="_blank">
+                    <h2>LinkedIn</h2>
+                </a>
+            </div>
         </div>
         <ViewName>
             <template #view-name>
@@ -22,5 +40,45 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.contact-container {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: flex;
+    gap: 6rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    box-sizing: border-box;
+    overflow-y: auto;
+    color: #fff;
 
+    &__section {
+        text-align: center;
+
+        h1 {
+            text-decoration: underline;
+            padding-bottom: 1rem;
+            cursor: default;
+        }
+
+        h2 {
+            padding-bottom: .5rem;
+            cursor: pointer;
+            color: #fff;
+            transition: color .2s ease-in-out;
+
+            &:hover {
+                color: #40826d;
+            }
+        }
+
+        a {
+            text-decoration: none;
+        }
+    }
+}
 </style>
