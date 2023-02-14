4355aa53216613f3ffd942f671660d9f15d3ff27
Tobias <tobias.weinlich@gmx.de>
2023-02-14 04:03:53 +0100

    Added Component to see on which link currently on

---
+++ src/components/ViewName.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="view-name">
+        <h2 class="view-name__text">
+            <slot name="view-name"></slot>
+        </h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.view-name {
+    position: absolute;
+    left: 2rem;
+    bottom: 2rem;
+    transform: rotate(-90deg);
+    transform-origin: 10%;
+    cursor: default;
+
+    &__text {
+        font-size: 4rem;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 0.1rem;
+        color: #fff;
+    }
+}
+</style>
