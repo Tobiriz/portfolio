4278caa155a99bfe727a53b4902a17c5b6beede0
Tobias <tobias.weinlich@gmx.de>
2023-02-15 00:33:47 +0100

    Styling

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -11,14 +11,17 @@ export default {
 
 <template>
     <div class="job">
+        <div class="job__date">
+            {{ job.date }}
+        </div>
         <div class="job__title">
             {{ job.title }}
         </div>
         <div class="job__company">
             {{ job.company }}
         </div>
-        <div class="job__date">
-            {{ job.date }}
+        <div class="job__location">
+            {{ job.location }}
         </div>
         <div class="job__description">
             {{ job.description }}
@@ -34,12 +37,21 @@ export default {
     align-items: flex-end;
     color: #fff;
     cursor: default;
+    padding-bottom: 2rem;
+    
+    &__date {
+        font-size: 1.5rem;
+        font-weight: 400;
+        letter-spacing: 0.1rem;
+    }
 
     &__title {
-        font-size: 2rem;
+        font-size: 2.5rem;
         font-weight: 700;
         text-transform: uppercase;
         letter-spacing: 0.1rem;
+        color: #40826d;
+        padding: .5rem 0;
     }
 
     &__company {
@@ -48,16 +60,16 @@ export default {
         letter-spacing: 0.1rem;
     }
 
-    &__date {
-        font-size: 1.5rem;
+    &__location {
+        font-size: 1.2rem;
         font-weight: 400;
-        text-transform: uppercase;
         letter-spacing: 0.1rem;
+        padding-bottom: 1rem;
     }
 
     &__description {
         text-align: end;
-        font-size: 1.5rem;
+        font-size: 1.2rem;
         font-weight: 400;
     }
 }
