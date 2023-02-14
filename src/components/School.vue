6c7a3f3fd236fffd89744950170325522de76e64
Tobias <tobias.weinlich@gmx.de>
2023-02-15 00:33:53 +0100

    Styling

--- src/components/School.vue
+++ src/components/School.vue
@@ -11,14 +11,17 @@ export default {
 
 <template>
     <div class="school">
-        <div class="school__title">
-            {{ school.title }}
-        </div>
         <div class="school__date">
             {{ school.date }}
         </div>
-        <div class="school__description">
-            {{ school.description }}
+        <div class="school__title">
+            {{ school.title }}
+        </div>
+        <div class="school__school">
+            {{ school.school }}
+        </div>
+        <div class="school__location">
+            {{ school.location }}
         </div>
     </div>
 </template>
@@ -30,25 +33,35 @@ export default {
     justify-content: center;
     color: #fff;
     cursor: default;
-
-    &__title {
-        font-size: 2rem;
-        font-weight: 700;
-        text-transform: uppercase;
-        letter-spacing: 0.1rem;
-    }
-
+    padding-bottom: 2rem;
+    
     &__date {
         font-size: 1.5rem;
         font-weight: 400;
-        text-transform: uppercase;
         letter-spacing: 0.1rem;
     }
 
-    &__description {
+    &__title {
+        font-size: 2.5rem;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 0.1rem;
+        color: #40826d;
+        padding: .5rem 0;
+        width: 50%;
+    }
+    
+    &__school {
         font-size: 1.5rem;
         font-weight: 400;
         letter-spacing: 0.1rem;
+    }
+
+    &__location {
+        font-size: 1.2rem;
+        font-weight: 400;
+        letter-spacing: 0.1rem;
+        padding-bottom: 1rem;
     }
 }
 </style>
