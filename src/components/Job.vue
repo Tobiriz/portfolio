b5a0e80fc8f8de1a2b0f5330dd2adb1e4b898a58
Tobias <tobias.weinlich@gmx.de>
2023-02-14 04:02:23 +0100

    Added a Job Component

---
+++ src/components/Job.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    props: {
+        job: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="job">
+        <div class="job__title">
+            {{ job.title }}
+        </div>
+        <div class="job__company">
+            {{ job.company }}
+        </div>
+        <div class="job__date">
+            {{ job.date }}
+        </div>
+        <div class="job__description">
+            {{ job.description }}
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.job {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    color: #fff;
+    cursor: default;
+
+    &__title {
+        font-size: 2rem;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 0.1rem;
+    }
+
+    &__company {
+        font-size: 1.5rem;
+        font-weight: 400;
+        letter-spacing: 0.1rem;
+    }
+
+    &__date {
+        font-size: 1.5rem;
+        font-weight: 400;
+        text-transform: uppercase;
+        letter-spacing: 0.1rem;
+    }
+
+    &__description {
+        text-align: end;
+        font-size: 1.5rem;
+        font-weight: 400;
+    }
+}
+</style>
