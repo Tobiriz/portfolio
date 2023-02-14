83d26108b281f9fef852984d198b175005bc2624
Tobias <tobias.weinlich@gmx.de>
2023-02-14 04:05:12 +0100

    Added a Projects Window

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,26 @@
+<script>
+import ViewName from '@/components/ViewName.vue'
+
+export default {
+    components: {
+        ViewName
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div>
+            Tobias Weinlich
+        </div>
+        <ViewName>
+            <template #view-name>
+                Projects
+            </template>
+        </ViewName>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
