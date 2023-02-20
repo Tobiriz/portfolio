1134f6f3f75e0f2c5d1bf6366f1e458f45659d07
Tobias <tobias.weinlich@gmx.de>
2023-02-20 03:37:51 +0100

    Added a Overlay Component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="overlay"></div>
+</template>
+
+<style lang="scss" scoped>
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--primary-color);
+    z-index: 100;
+}
+</style>
