cd5d89e7d4f781815046392978580fe9d0a01f4d
Tobias <tobias.weinlich@gmx.de>
2023-02-20 03:39:21 +0100

    Created the Projects presentation

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,26 +1,43 @@
 <script>
 import ViewName from '@/components/ViewName.vue'
+import Project from '@/components/Project.vue'
+import { useSettingsStore } from '@/stores/settingsStore'
 
 export default {
     components: {
-        ViewName
-    }
+        ViewName,
+        Project
+    },
+
+    setup() {
+        const store = useSettingsStore()
+        return { store }
+    },
 }
 </script>
 
 <template>
     <div>
-        <div>
-            Tobias Weinlich
+        <div class="projects-container">
+            <Project v-for="project in store.projects" :key="project.id" :project="project" />
         </div>
         <ViewName>
             <template #view-name>
-                Projects
+                {{ store.projectsViewName }}
             </template>
         </ViewName>
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+.projects-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+    color: var(--secondary-color);
+    cursor: default;
+}
 </style>
