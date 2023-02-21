717d1dc6ad6eddd124f02ae6a3968c2d82027551
Tobias <tobias.weinlich@gmx.de>
2023-02-21 03:25:14 +0100

    Created the Contact Store

---
+++ src/stores/landingPageStore.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+import { useSettingsStore } from './settingsStore'
+
+export const useLandingPageStore = defineStore('LandingPageStore', {
+    state: () => {
+        return {
+            settings: useSettingsStore(),
+
+            en: {
+                name: 'Tobias Weinlich',
+                occupation: 'Software Developer',
+            },
+
+            de: {
+                name: 'Tobias Weinlich',
+                occupation: 'Softwareentwickler',
+            }
+        }
+    },
+
+    getters: {
+        name() {
+            if (this.settings.languageEnglish) {
+                return this.en.name
+            } else if (this.settings.languageGerman) {
+                return this.de.name
+            }
+        },
+        occupation() {
+            if (this.settings.languageEnglish) {
+                return this.en.occupation
+            } else if (this.settings.languageGerman) {
+                return this.de.occupation
+            }
+        }
+    }
+})
