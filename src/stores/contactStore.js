d363c212326a146754906446b3a1369ee5e38a59
Tobias <tobias.weinlich@gmx.de>
2023-02-21 03:25:08 +0100

    Created the Contact Store

---
+++ src/stores/contactStore.js
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+import { useSettingsStore } from './settingsStore'
+
+export const useContactStore = defineStore('ContactStore', {
+    state: () => {
+        return {
+            settings: useSettingsStore(),
+
+            en: {
+                viewName: 'Contact',
+                email: 'Email',
+                emailAddress: 'contact@tobiasweinlich.com',
+                media: 'Media',
+            },
+
+            de: {
+                viewName: 'Kontakt',
+                email: 'E-Mail',
+                emailAddress: 'kontakt@tobiasweinlich.com',
+                media: 'Medien',
+            }
+        }
+    },
+
+    getters: {
+        viewName() {
+            if (this.settings.languageEnglish) {
+                return this.en.viewName
+            } else if (this.settings.languageGerman) {
+                return this.de.viewName
+            }
+        },
+        email() {
+            if (this.settings.languageEnglish) {
+                return this.en.email
+            } else if (this.settings.languageGerman) {
+                return this.de.email
+            }
+        },
+        emailAddress() {
+            if (this.settings.languageEnglish) {
+                return this.en.emailAddress
+            } else if (this.settings.languageGerman) {
+                return this.de.emailAddress
+            }
+        },
+        media() {
+            if (this.settings.languageEnglish) {
+                return this.en.media
+            } else if (this.settings.languageGerman) {
+                return this.de.media
+            }
+        },
+    }
+})
