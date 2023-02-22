44148a74953a9798c596525cdd1fe648807edb67
Tobias <tobias.weinlich@gmx.de>
2023-02-22 04:21:57 +0100

    Added Transition

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -23,9 +23,11 @@ export default {
             <h3>{{ title }}</h3>
         </div>
 
+        <Transition>
         <div class="skill__content" v-show="isActive">
-            <p>{{ content }}</p>
-        </div>
+                <p>{{ content }}</p>
+            </div>
+        </Transition>
     </div>
 </template>
 
@@ -64,5 +66,19 @@ export default {
 
 .isActive {
     color:  var(--secondary-color);
+}
+
+.v-enter-active, .v-leave-active {
+    transition: all 0.5s;
+}
+
+.v-enter-from, .v-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+.v-enter-to, .v-leave-from {
+    opacity: 1;
+    transform: translateX(0%);
 }
 </style>
