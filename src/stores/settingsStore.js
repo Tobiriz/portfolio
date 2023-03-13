0007c2555783bb0e7e3f490d0a520d3c985f1a60
Tobias <tobias.weinlich@gmx.de>
2023-03-13 23:28:32 +0100

    Adjusted colors, favicon change and localStorage

--- src/stores/settingsStore.js
+++ src/stores/settingsStore.js
@@ -8,6 +8,7 @@ export const useSettingsStore = defineStore('SettingsStore', {
             darkMode: true,
             lightMode: false,
             overlayActive: false,
+            contentUpdate: false,
         }
     },
 
@@ -15,27 +16,35 @@ export const useSettingsStore = defineStore('SettingsStore', {
         setDarkMode() {
             this.lightMode = false
             setTimeout(() => {this.darkMode = true}, 150)
-            document.documentElement.style.setProperty('--primary-color', '#191919')
-            document.documentElement.style.setProperty('--secondary-color', '#ccc')
-            document.documentElement.style.setProperty('--secondary-color-light', '#cccccc88')
-            document.documentElement.style.setProperty('--canvas-color', '#666')
-            document.documentElement.style.setProperty('--accent-color', '#3A98B9')
-            document.documentElement.style.setProperty('--title-card-color', '#333')
-            document.documentElement.style.setProperty('--blob-color-1', '#95BDFF55')
-            document.documentElement.style.setProperty('--blob-color-2', '#DFFFD855')
+            document.documentElement.style.setProperty('--primary-color', 'var(--c-black)')
+            document.documentElement.style.setProperty('--secondary-color', 'var(--c-white-mute)')
+            document.documentElement.style.setProperty('--secondary-color-light', 'var(--c-white-mute-transparent)')
+            document.documentElement.style.setProperty('--canvas-color', 'var(--c-grey)')
+            document.documentElement.style.setProperty('--accent-color', 'var(--c-blue-light)')
+            document.documentElement.style.setProperty('--title-card-color', 'var(--c-black-soft)')
+            document.documentElement.style.setProperty('--blob-color-1', 'var(--c-blue-dark)')
+            document.documentElement.style.setProperty('--blob-color-2', 'var(--c-greyish)')
+            this.setFavicon('./public/favicon-dark.ico')
+
+            //Store the current theme in local storage
+            localStorage.setItem('theme', 'dark')
         },
         
         setLightMode() {
             this.darkMode = false
             setTimeout(() => {this.lightMode = true}, 150)
-            document.documentElement.style.setProperty('--primary-color', '#eee')
-            document.documentElement.style.setProperty('--secondary-color', '#191919')
-            document.documentElement.style.setProperty('--secondary-color-light', '#191919aa')
-            document.documentElement.style.setProperty('--canvas-color', '#999')
-            document.documentElement.style.setProperty('--accent-color', '#C74B50')
-            document.documentElement.style.setProperty('--title-card-color', '#bbb')
-            document.documentElement.style.setProperty('--blob-color-1', '#712B7555')
-            document.documentElement.style.setProperty('--blob-color-2', '#D49B5455')
+            document.documentElement.style.setProperty('--primary-color', 'var(--c-white)')
+            document.documentElement.style.setProperty('--secondary-color', 'var(--c-black)')
+            document.documentElement.style.setProperty('--secondary-color-light', 'var(--c-black-transparent)')
+            document.documentElement.style.setProperty('--canvas-color', 'var(--c-grey-soft)')
+            document.documentElement.style.setProperty('--accent-color', 'var(--c-red)')
+            document.documentElement.style.setProperty('--title-card-color', 'var(--c-white-soft)')
+            document.documentElement.style.setProperty('--blob-color-1', 'var(--c-violet-dark)')
+            document.documentElement.style.setProperty('--blob-color-2', 'var(--c-orange-dark)')
+            this.setFavicon('./public/favicon-light.ico')
+
+            //Store the current theme in local storage
+            localStorage.setItem('theme', 'light')
         },
 
         setLanguageGerman() {
@@ -45,6 +54,9 @@ export const useSettingsStore = defineStore('SettingsStore', {
                 this.languageGerman = true
                 this.overlayActive = false
             }, 600)
+
+            //Store the current language in local storage
+            localStorage.setItem('language', 'de')
         },
 
         setLanguageEnglish() {
@@ -54,6 +66,36 @@ export const useSettingsStore = defineStore('SettingsStore', {
                 this.languageEnglish = true
                 this.overlayActive = false
             }, 600)
+
+            //Store the current language in local storage
+            localStorage.setItem('language', 'en')
+        },
+
+        contentUpdateTrigger() {
+            this.contentUpdate = true
+            setTimeout(() => {this.contentUpdate = false}, 100)
+        },
+
+        setFavicon(img) {
+            let link = document.querySelector("link[rel*='icon']") || document.createElement('link')
+            link.type = 'image/x-icon'
+            link.rel = 'shortcut icon'
+            link.href = img
+            document.getElementsByTagName('head')[0].appendChild(link)
+        },
+
+        readLocalStorage() {
+            if (localStorage.getItem('theme') === 'dark') {
+                this.setDarkMode()
+            } else if (localStorage.getItem('theme') === 'light') {
+                this.setLightMode()
+            }
+
+            if (localStorage.getItem('language') === 'de') {
+                this.setLanguageGerman()
+            } else if (localStorage.getItem('language') === 'en') {
+                this.setLanguageEnglish()
+            }
         }
     },
 })
