dc1ecd6ce17bd85a78bad37bbee4772b3d412d76
Tobias <tobias.weinlich@gmx.de>
2023-03-13 23:29:09 +0100

    Adjusted cursor behvaior and made hacker effect

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -14,6 +14,18 @@ export default {
         return { landingPage }
     },
 
+    data() {
+        return {
+            firstName: null,
+            lastName: null,
+            firstNameText: null,
+            lastNameText: null,
+            letters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            interval: null,
+            endEffect: false
+        }
+    },
+
     mounted() {
         const settings = document.getElementsByClassName('landing-page-settings')[0]
         const titleCard = document.getElementsByClassName('title-card')[0]
@@ -21,14 +33,158 @@ export default {
         const titleCardBottom = titleCard.offsetTop
 
         settings.style.top = (titleCardBottom + titleCardHeight) + 'px'
+
+        const link = document.getElementById('routerLink')
+
+        this.firstName = document.getElementById('first-name')
+        this.lastName = document.getElementById('last-name')
+
+        this.firstNameText = document.getElementById('first-name').innerText.split('')
+        this.lastNameText = document.getElementById('last-name').innerText.split('')
+
+        link.addEventListener('mouseenter', () => {
+            if (!this.endEffect) {
+                this.startHackerEffect()
+                this.handleMouseOver()
+            }
+        })
+
+        link.addEventListener('mouseleave', () => {
+            if (!this.endEffect) {
+                this.stopHackerEffect()
+                this.handleMouseLeave()
+            }
+        })
+
+        link.addEventListener('click', () => {
+            this.endHackerEffect()
+            this.handleMouseClick()
+        })
     },
+
+    unmounted() {
+        const link = document.getElementById('routerLink')
+
+        link.removeEventListener('mouseenter', () => {
+            if (!this.endEffect) {
+                this.startHackerEffect()
+                this.handleMouseOver()
+            }
+        })
+
+        link.removeEventListener('mouseleave', () => {
+            if (!this.endEffect) {
+                this.stopHackerEffect()
+                this.handleMouseLeave()
+            }
+        })
+
+        link.removeEventListener('click', () => {
+            this.endHackerEffect()
+            this.handleMouseClick()
+        })
+
+        this.handleMouseLeave()
+    },
+
+    methods: {
+        startHackerEffect() {
+            this.interval = setInterval(() => {
+                this.firstName.innerText = this.firstName.innerText.split('')
+                    .map(letter => this.letters[Math.floor(Math.random() * this.letters.length)])
+                    .join('')
+
+                this.lastName.innerText = this.lastName.innerText.split('')
+                    .map(letter => this.letters[Math.floor(Math.random() * this.letters.length)])
+                    .join('')
+            }, 20)
+        },
+
+        stopHackerEffect() {
+            clearInterval(this.interval)
+        },
+
+        endHackerEffect() {
+            if (this.endEffect) {
+                this.$router.push('/portfolio')
+            }
+
+            this.stopHackerEffect()
+            this.endEffect = true
+
+            const nameLength = this.firstNameText.length + this.lastNameText.length
+            let iterations = 0
+            let count = 0
+
+            console.log("endHackerEffect")
+
+            this.interval = setInterval(() => {
+                this.firstName.innerText = this.firstName.innerText.split('')
+                    .map((letter, index) => {
+                        if (index < iterations) {
+                            return this.firstNameText[index]
+                        }
+                        return this.letters[Math.floor(Math.random() * this.letters.length)]
+                    })
+                    .join('')
+
+                if (iterations >= this.firstNameText.length) {
+                    this.lastName.innerText = this.lastName.innerText.split('')
+                        .map((letter, index) => {
+                            if (index < iterations - this.firstNameText.length) {
+                                return this.lastNameText[index]
+                            }
+                            return this.letters[Math.floor(Math.random() * this.letters.length)]
+                        })
+                        .join('')
+                } else {
+                    this.lastName.innerText = this.lastName.innerText.split('')
+                        .map(letter => this.letters[Math.floor(Math.random() * this.letters.length)])
+                        .join('')
+                }
+
+                if (iterations >= nameLength) {
+                    clearInterval(this.interval)
+                    setTimeout(() => {
+                        this.$router.push('/portfolio')
+                    }, 500)
+                }
+
+                count++
+                if (count === 10) {
+                    count = 0
+                    iterations++
+                }
+            }, 10)
+
+            const occupation = document.getElementsByClassName('title-card__content__occupation')[0]
+        },
+
+        handleMouseOver() {
+            const cursor = document.getElementById('cursor')
+            cursor.classList.add('cursor--active')
+        },
+
+        handleMouseLeave() {
+            const cursor = document.getElementById('cursor')
+            cursor.classList.remove('cursor--active')
+        },
+
+        handleMouseClick() {
+            const cursor = document.getElementById('cursor')
+            cursor.classList.add('cursor--click')
+            setTimeout(() => {
+                cursor.classList.remove('cursor--click')
+            }, 300)
+        },
+    }
 }
 </script>
 
 <template>
     <div class="body">
         <Settings class="landing-page-settings"></Settings>
-        <RouterLink to="/portfolio">
+        <div id="routerLink">
             <div
             class="title-card"
             @mouseenter="showChevron"
@@ -37,11 +193,15 @@ export default {
             
                 <div class="title-card__content">
                     <div class="title-card__content__name">
-                        <h1>{{ landingPage.name }}</h1>
+                        <h1>
+                            <span id="first-name">Tobias</span>
+                            <span>&nbsp;</span>
+                            <span id="last-name">Weinlich</span>
+                        </h1>
                     </div>
                     
                     <div class="title-card__content__occupation">
-                        <h2>{{ landingPage.occupation }}</h2>
+                        <h2 id="title-card-occupation">{{ landingPage.occupation }}</h2>
                     </div>
                 </div>
                 
@@ -51,11 +211,18 @@ export default {
                     class="title-card__chevron"
                 />
             </div>
-        </RouterLink>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Martian+Mono&display=swap');
+
+#first-name,
+#last-name {
+    font-family: 'Martian Mono', monospace;
+}
+
 .body {
     display: flex;
     flex-direction: column;
@@ -64,12 +231,12 @@ export default {
     align-items: center;
     width: 100vw;
     height: 100vh;
-    background-color: var(--primary-color);
     z-index: 3;
 }
 
 .landing-page-settings {
     position: absolute;
+    font-size: 1.2rem;
     z-index: 4;
 }
 
@@ -151,7 +318,6 @@ export default {
 }
 
 a {
-    cursor: default;
     text-decoration: none;
     color: var(--secondary-color);
 }
