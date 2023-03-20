4dc94d527fe069304e9c4486791f9561beb99daa
Tobias <tobias.weinlich@gmx.de>
2023-03-20 03:15:25 +0100

    added window scroll behavior

--- src/views/SkillsetView.vue
+++ src/views/SkillsetView.vue
@@ -1,6 +1,7 @@
 <script>
 import Skill from "@/components/Skill.vue";
 import { useSkillsetStore } from "@/stores/skillsetStore";
+import { useSettingsStore } from "@/stores/settingsStore";
 
 export default {
   components: {
@@ -9,7 +10,8 @@ export default {
 
   setup() {
     const skillset = useSkillsetStore();
-    return { skillset };
+    const settings = useSettingsStore();
+    return { skillset, settings };
   },
 
   data() {
@@ -18,9 +20,76 @@ export default {
     };
   },
 
+  watch: {
+    overlayActive() {
+      this.activeSkill = null;
+      setTimeout(() => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }, 300);
+    },
+  },
+
   methods: {
     toggleSkill(index) {
+      let skillHeightBefore = 0;
+      let contentHeightBefore = 0;
+      if (this.activeSkill !== null) {
+        skillHeightBefore =
+          document.getElementsByClassName("skill")[this.activeSkill]
+            .clientHeight;
+        contentHeightBefore =
+          document.getElementsByClassName("skill__content")[this.activeSkill]
+            .clientHeight;
+      }
+
       this.activeSkill = this.activeSkill === index ? null : index;
+
+      if (this.activeSkill === null) {
+        return;
+      }
+
+      const vh = document.documentElement.clientHeight;
+      const top = window.pageYOffset || document.documentElement.scrollTop;
+      const bottom = top + vh;
+
+      setTimeout(() => {
+        const skill = document.getElementsByClassName("skill")[index];
+        const skillHeight = skill.clientHeight;
+
+        if (skillHeight > vh) return;
+
+        if (skill.offsetTop - 20 < top) {
+          window.scrollTo({
+            top: skill.offsetTop - 60,
+            behavior: "smooth",
+          });
+          return;
+        }
+
+        if (
+          skillHeightBefore === 0 &&
+          skill.offsetTop + skillHeight + 20 > bottom
+        ) {
+          window.scrollTo({
+            top: skill.offsetTop + skillHeight - vh + 60,
+            behavior: "smooth",
+          });
+          return;
+        }
+
+        if (
+          skillHeightBefore !== 0 &&
+          skill.offsetTop + skillHeight - contentHeightBefore + 20 > bottom
+        ) {
+          window.scrollTo({
+            top: skill.offsetTop - contentHeightBefore + skillHeight - vh + 60,
+            behavior: "smooth",
+          });
+        }
+      }, 10);
     },
   },
 
@@ -30,33 +99,42 @@ export default {
         a.title.localeCompare(b.title)
       );
     },
+
+    overlayActive() {
+      return this.settings.overlayIsActive;
+    },
   },
 };
 </script>
 
 <template>
-  <div class="skillsets">
-    <Skill
-      v-for="(skill, index) in skills"
-      :key="index"
-      :title="skill.title"
-      :content="skill.content"
-      :is-active="this.activeSkill === index"
-      @toggle="toggleSkill(index)"
-    />
+  <div>
+    <div class="skillsets" v-for="(skill, index) in skills" :key="index">
+      <Skill
+        :title="skill.title"
+        :content="skill.content"
+        :is-active="this.activeSkill === index"
+        @toggle="toggleSkill(index)"
+      />
+      <div class="line" v-if="index !== skills.length - 1"></div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .skillsets {
   padding: 2rem;
-  padding-bottom: 10rem;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  gap: 2.5rem;
+  gap: 3rem;
   box-sizing: border-box;
-  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.line {
+  border-top: 1px solid var(--color-secondary-light);
+  width: clamp(100px, 15%, 300px);
 }
 
 @media screen and (max-width: 1279px) {
